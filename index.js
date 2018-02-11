eb1ea63f547d5ed1328bd0aea047d5367b8ada6e
mustafa hussain <mustafa.hussain93@gmail.com>
2018-02-12 00:09:55 +0200

    update to bst only

--- index.js
+++ index.js
@@ -1,24 +1,3 @@
 'use strict';
 
-const BST = require('./src/BinarySearchTree');
-
-const bst = new BST();
-
-bst.add(15);
-bst.add(17);
-bst.add(20);
-bst.add(25);
-bst.add(5);
-bst.add(2);
-bst.add(4);
-bst.add(7);
-bst.add(9);
-bst.add(1);
-
-//                 15
-//               /   \
-//            5         17
-//          /    \        \
-//        2       7         20
-//      /   \       \         \
-//    1       4       9         25
+module.exports = require('./src/BinarySearchTree');
--- src/AVLTree.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-const _ = require('lodash');
-
-class AVLTree {
-
-}
-module.exports = AVLTree;
